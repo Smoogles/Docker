13713d56fbca28ec6cc29ecc373ef7ce5349eebc
james <chillin.jimmy99@gmail.com>
2022-07-26 17:55:08 +1000

    first commit

---
+++ HelloWorldTest1/HelloWorldTest1/Program.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using System;
+
+namespace HelloWorldTest1
+{
+    public class Startup
+    {
+        public void Configure(IApplicationBuilder applicationBuilder, IHostingEnvironment hostingEnvironment)
+        {
+            applicationBuilder.Run(async context =>
+            {
+                await context.Response.WriteAsync("Hello World");
+            });
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebHost.CreateDefaultBuilder()
+            .UseStartup<Startup>()
+            .UseKestrel()
+            .UseUrls("http://0.0.0.0:5050")
+            .Build()
+            .Run();
+        }
+    }
+}
+}
